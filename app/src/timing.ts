8559bea5be4733f11d0b90998530cecef148bcc9
conni <cbuschka@gmail.com>
2021-08-01 18:42:50 +0200

    Added generic type information to timing wrapper.

--- app/src/timing.ts
+++ app/src/timing.ts
@@ -1,20 +1,15 @@
-const endTiming = (start: Date, result: any, type: any) => {
+const endTiming = <T>(start: Date, result: T, type: any): T => {
   const end: Date = new Date();
   const duration: number = end.getTime() - start.getTime();
   // tslint:disable-next-line:no-console
-  console.log("TIMING: %s", JSON.stringify({
-    type,
-    start,
-    end,
-    durationMillis: duration
-  }));
+  console.log("TIMING: %s", JSON.stringify({type, start, end, durationMillis: duration}));
   return result;
 }
 
-export const withTiming = (f: any, type: any) => {
+export const withTiming = <T>(f: Function, type: any): T => {
   const start = new Date();
   const result = f();
-  if (result && result.then) {
+  if (result && "then" in result) {
     return result.finally((value: any) => {
       return endTiming(start, value, type);
     });
